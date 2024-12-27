a7398e60cd173f53870365c51d677b34aad5db05
Vincent Jade Alivio <vincentjade.vj@gmail.com>
2024-12-27 09:29:53 +0800

    datebase backup

--- POS/Database/Product - Add Column (ExpiryDate).sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE Product 
-ADD	ExpiryDate DATE
-
-
--- UPDATE Product SET ExpiryDate = '2025-12-31'
--- POS/Database/Product - Add Column.sql
+++
@@ -1,8 +0,0 @@
-
-
-
-ALTER TABLE Product 
-ADD	MinimumPrice MONEY ,
-	MaximumPrice MONEY 
-
---UPDATE Product SET MinimumPrice = Product.OriginalPrice,MaximumPrice=Product.FinalPrice
